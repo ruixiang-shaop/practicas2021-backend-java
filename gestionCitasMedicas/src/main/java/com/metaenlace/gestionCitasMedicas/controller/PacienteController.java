9e8971a9b9e99499a7e35e01a16f3b5545347752
ruixiang-shaop <ruixiang.shaop@um.es>
2021-03-01 09:53:17 +0100

    Paciente register working

--- gestionCitasMedicas/src/main/java/com/metaenlace/gestionCitasMedicas/controller/PacienteController.java
+++ gestionCitasMedicas/src/main/java/com/metaenlace/gestionCitasMedicas/controller/PacienteController.java
@@ -19,7 +19,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.metaenlace.gestionCitasMedicas.dto.MedicoRegistroDTO;
 import com.metaenlace.gestionCitasMedicas.dto.PacienteDTO;
+import com.metaenlace.gestionCitasMedicas.dto.PacienteRegistroDTO;
+import com.metaenlace.gestionCitasMedicas.entity.Medico;
 import com.metaenlace.gestionCitasMedicas.entity.Paciente;
 import com.metaenlace.gestionCitasMedicas.service.IPacienteService;
 
@@ -53,9 +56,9 @@ public class PacienteController {
     @PostMapping("/pacientes/add")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public PacienteDTO addPaciente(@RequestBody PacienteDTO pacDTO) {
+    public PacienteDTO addPaciente(@RequestBody PacienteRegistroDTO pacRegDTO) {
     	try {
-    		Paciente pac = convertToEntity(pacDTO);
+    		Paciente pac = convertRegistroToEntity(pacRegDTO);
     		Paciente createPac = pacienteService.save(pac);
     		return convertToDTO(createPac);
     	} catch (DataAccessException e) {
@@ -92,6 +95,11 @@ public class PacienteController {
     
     private Paciente convertToEntity(PacienteDTO pacDTO) {
     	Paciente pac = modelMapper.map(pacDTO, Paciente.class);
+        return pac;
+    }
+    
+    private Paciente convertRegistroToEntity(PacienteRegistroDTO pacRegDTO) {
+    	Paciente pac = modelMapper.map(pacRegDTO, Paciente.class);
         return pac;
     }
 }
