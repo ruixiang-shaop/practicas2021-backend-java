3901c57021a89ff7fd732d57e867ee5d81411ba5
ruixiang-shaop <ruixiang.shaop@um.es>
2021-03-01 09:59:51 +0100

    Remove unused imports

--- gestionCitasMedicas/src/main/java/com/metaenlace/gestionCitasMedicas/controller/PacienteController.java
+++ gestionCitasMedicas/src/main/java/com/metaenlace/gestionCitasMedicas/controller/PacienteController.java
@@ -19,10 +19,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.metaenlace.gestionCitasMedicas.dto.MedicoRegistroDTO;
 import com.metaenlace.gestionCitasMedicas.dto.PacienteDTO;
 import com.metaenlace.gestionCitasMedicas.dto.PacienteRegistroDTO;
-import com.metaenlace.gestionCitasMedicas.entity.Medico;
 import com.metaenlace.gestionCitasMedicas.entity.Paciente;
 import com.metaenlace.gestionCitasMedicas.service.IPacienteService;
 
